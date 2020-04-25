abf0f246bc1ec3387795597edeb5ef6ab955709b
Pronin Denis <denis.pronin@sibers.com>
2020-04-25 15:29:18 +0700

    files markup

--- src/components/order/Files.js
+++ src/components/order/Files.js
@@ -6,26 +6,57 @@ import ImageIcon from '@material-ui/icons/Image';
 import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '24px',
+    marginBottom: '28px'
+  },
+  heading: {
+    fontWeight: 500,
+    fontSize: '11px',
+    lineHeight: '13px',
+    letterSpacing: '0.02em',
+    color: 'rgba(0, 0, 0, 0.4)',
+    marginBottom: '16px'
+  },
   chip: {
-    borderRadius: 0
+    borderRadius: 0,
+    backgroundColor: 'rgba(26, 35, 126, 0.1)',
+    marginRight: '16px'
+  },
+  chipIcon: {
+    color: '#009688'
   }
 }));
 
 function Files() {
   const classes = useStyles();
+  const files = [
+    {
+      fileName: 'Site photo'
+    },
+    {
+      fileName: 'Louver design..'
+    }
+  ];
   
   return (
-    <div>
-      <Typography variant="h6" gutterBottom>
-        FILES (2)
+    <div className={classes.root}>
+      <Typography variant="h6" gutterBottom className={classes.heading}>
+        FILES ({files.length})
       </Typography>
   
       <div>
-        <Chip
-          icon={<ImageIcon />}
-          label="Site photo"
-          className={classes.chip}
-        />
+        {files.map((file, i) => {
+          return (
+            <Chip
+              key={`chip-${i}`}
+              icon={<ImageIcon fontSize="small" />}
+              label={file.fileName}
+              className={classes.chip}
+              classes={{icon: classes.chipIcon}}
+            />
+          );
+        })}
       </div>
       
     </div>
