86b469e603bd1ce866b53933b6097e1cef7b9a22
Pronin Denis <denis.pronin@sibers.com>
2020-04-25 15:42:18 +0700

    Updates

---
+++ precache-manifest.f927e9dfd872e7e8f48f0a705cd9a029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6837c983b976c2732263f680fa35722",
+    "url": "//my-app/index.html"
+  },
+  {
+    "revision": "534fc39f2670c590a39a",
+    "url": "//my-app/static/css/main.e1305a31.chunk.css"
+  },
+  {
+    "revision": "ea82190770bc2c96de0b",
+    "url": "//my-app/static/js/2.e87dd9b5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "//my-app/static/js/2.e87dd9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534fc39f2670c590a39a",
+    "url": "//my-app/static/js/main.f37831d9.chunk.js"
+  },
+  {
+    "revision": "034c6c5f6c6f2f40324e",
+    "url": "//my-app/static/js/runtime-main.8f467613.js"
+  },
+  {
+    "revision": "9f27cedf3970ca3365bcef07bc352583",
+    "url": "//my-app/static/media/street.9f27cedf.jpg"
+  }
+]);
